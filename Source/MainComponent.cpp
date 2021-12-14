feacf14b466571ed9909779b403afff4d2f92bb6
Hadi <saleh.hadiyasser@hotmail.com>
2021-12-14 11:30:23 +0000

    added .DS_Store to gitignore

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -23,7 +23,5 @@ void MainComponent::paint (juce::Graphics& g)
 
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+   // toggleButton.setBounds(10, 10, 30, 30);
 }
