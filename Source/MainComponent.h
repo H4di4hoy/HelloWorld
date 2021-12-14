86a8a1b2cbbd48f7bea5faba012d8839dee044ef
Hadi <saleh.hadiyasser@hotmail.com>
2021-12-14 13:53:30 +0000

    Added a toggle button

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -19,8 +19,8 @@ public:
     void resized() override;
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
+    
+    juce::ToggleButton toggleButton;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
